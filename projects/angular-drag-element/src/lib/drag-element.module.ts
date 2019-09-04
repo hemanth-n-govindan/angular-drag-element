9f2f9b709de2970eef7471b69b6184f32fa5c6d5
Hemanth N Govindan <Hemanth.NG612@gmail.com>
2019-09-04 14:40:16 +0530

    reanme

---
+++ projects/angular-drag-element/src/lib/drag-element.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { DragElementDirective } from "./directives/drag-element/drag-element.directive";
+
+@NgModule({
+  declarations: [DragElementDirective],
+  imports: [],
+  exports: [DragElementDirective]
+})
+export class DragElementModule {}
---
+++ projects/angular-drag-element/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-drag-element
+ */
+
+export * from "./lib/directives/drag-element/drag-element.directive";
+export * from "./lib/drag-element.module";
